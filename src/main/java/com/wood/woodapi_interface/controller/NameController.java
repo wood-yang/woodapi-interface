cc539c92cf37b470e2f9bbafcc200aabf5b6c3e0
wood <wood13590@gmail.com>
2024-08-30 02:17:39 +0800

    update 实现三个公共接口

--- src/main/java/com/wood/woodapi_interface/controller/NameController.java
+++ src/main/java/com/wood/woodapi_interface/controller/NameController.java
@@ -32,35 +32,6 @@ public class NameController {
 
     @PostMapping("/user")
     public String getUsernameByPost(@RequestBody User user, HttpServletRequest request, HttpServletResponse response) {
-        String accessKey = request.getHeader("accessKey");
-        String nonce = request.getHeader("nonce");
-        String timestamp = request.getHeader("timestamp");
-        String sign = request.getHeader("sign");
-        String body = request.getHeader("body");
-        // todo 实际情况应该是去数据库中查看用户是否具有accessKey
-//        if (!"wood".equals(accessKey)) {
-//            throw new RuntimeException("无权限");
-//        }
-        if (Long.parseLong(nonce) > 10000) {
-            throw new RuntimeException("无权限");
-        }
-        // 时间和当前时间不能超过 5 min
-        long current = System.currentTimeMillis() / 1000;
-        if (current - Long.parseLong(timestamp) > 60 * 5) {
-            throw new RuntimeException("请求已超时");
-        }
-        // todo 实际情况中是从服务器查出 secertKey
-//        if (!SignUtils.getSign(body, "abc").equals(sign)) {
-//            throw new RuntimeException("无权限");
-//        }
-
-
-        String result = "POST 用户名是" + user.getUsername();
-        // 调用成功，次数+1
-
-        response.addHeader("game", "LOL");
-        response.addHeader("clothes", "T-shirt");
-
-        return result;
+        return "POST 用户名是" + user.getUsername();
     }
 }
